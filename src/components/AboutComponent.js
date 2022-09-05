dd22cf654d874af31696febc8c3cf272f7feb3de
rbanerjee78 <rahulorama@gmail.com>
2022-09-05 10:40:11 +0530

    Leaders Fetch Redux

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,42 +1,47 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 
 
-function RenderLeader({leader}){      
-    return(
+function RenderLeader({ leader }) {
+    return (
+        <Stagger in>
         <div key={leader.id} className="col-12 mt-5">
-        <Media tag="li">
-         <Media left middle>
-           <Media object src={leader.image} alt={leader.name} />
-         </Media>
-         <Media body className="ml-5">
-          <Media heading>{leader.name}</Media>
-          <p>{leader.designation}</p>
-          <p>{leader.description}</p>
-         </Media>
-        </Media>
-       </div>
-         )
+            <Fade in><Media tag="li">
+                <Media left middle>
+                    <Media object src={baseUrl + leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media></Fade>
+        </div>
+        </Stagger>
+    )
 }
 
 
 
-const About = (props)=>{   
+const About = (props) => {
+
+
     const leaders = props.leaders.map((leader) => {
-       
+
         return (
-            <div className="col-12  m-1"  key={leader.id}>
-                <RenderLeader leader={leader}  />
+
+            <div className="col-12  m-1" key={leader.id}>
+                <RenderLeader leader={leader} />
             </div>
         );
-    });
-
-    
+        });
 
 
-    return(
+    return (
         <div className="container">
             <div className="row">
                 <Breadcrumb>
@@ -46,7 +51,7 @@ const About = (props)=>{
                 <div className="col-12">
                     <h3>About Us</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row row-content">
                 <div className="col-12 col-md-6">
@@ -78,8 +83,8 @@ const About = (props)=>{
                                 <p className="mb-0">You better cut the pizza in four pieces because
                                     I'm not hungry enough to eat six.</p>
                                 <footer className="blockquote-footer">Yogi Berra,
-                                <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
-                                    P. Pepe, Diversion Books, 2014</cite>
+                                    <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                                        P. Pepe, Diversion Books, 2014</cite>
                                 </footer>
                             </blockquote>
                         </CardBody>
@@ -91,16 +96,12 @@ const About = (props)=>{
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                {leaders}
+                    {leaders}
                 </div>
             </div>
         </div>
     );
-    }
-
-
-
-
+}
 
 
 
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -151,3 +151,41 @@ export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
 });
+
+//LEADERS
+export const fetchLeaders = () => (dispatch) => {
+
+    dispatch(leadersLoading());
+
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADER_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADER_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (promos) => ({
+    type: ActionTypes.ADD_LEADER,
+    payload: promos
+});
